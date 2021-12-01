e4fb1111b7635b889bdddcc47535187ebd1baf72
GiuliaSossai <giulia.sossai@yahoo.it>
2021-12-01 12:31:19 +0100

    init project

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+   <header>
+      <input 
+         type="text" 
+         placeholder="cerca"
+         v-model="search"
+         @keyup.enter="getInput"
+      >
+      <button>cerca</button>
+   </header>
+</template>
+
+<script>
+export default {
+   name: 'Header',
+   components: {
+      
+   },
+   data(){
+      return {
+         search: ''
+      }
+   },
+   methods: {
+      getInput(){
+         console.log(this.search);
+      }
+   }
+
+}
+</script>
+
+<style lang="scss">
+   @import '../assets/style/vars.scss';
+   @import '../assets/style/generals.scss';
+
+   header {
+      font: $main-font;
+   }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+     main
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+   @import '../assets/style/vars.scss';
+   @import '../assets/style/generals.scss';
+
+</style>
